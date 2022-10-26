84a86b8f15cf553a51072131edab625a0fb19e2f
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2022-10-26 14:22:44 -0400

    Commit for docs

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\src\\css\\custom.css'),
+];
---
+++ docusaurus.config.js
@@ -0,0 +1,113 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Control-Wrapper',
+  tagline: 'Control-Wrapper is an open source API Wrapper written in Python for the ControlPanel API (CPGG).',
+  url: 'https://control-wrapper.github.io',
+  baseUrl: '/docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  deploymentBranch: 'gh-pages',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Control-Wrapper', // Usually your GitHub org/user name.
+  projectName: 'Control-Wrapper', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Control-Wrapper/Control-Wrapper',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Control-Wrapper',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          {
+            type: 'doc',
+            docId: 'index',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/Control-Wrapper/Control-Wrapper',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/Bq5qK6ZG7F',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Control-Wrapper/Control-Wrapper',
+              }
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Control-Wrapper, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
