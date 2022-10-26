85ce07d9d0da01fff2a15eeb7f03c6418c0c7ff5
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2022-10-26 14:34:30 -0400

    Clean branch

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\Fernando\\Documents\\GitHub\\docs\\files\\src\\css\\custom.css'),
-];
